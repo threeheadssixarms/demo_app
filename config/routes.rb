02581589a3ac84469233b411eacdd6c890bdae28
Thao <lepgthao@gmail.com>
2016-09-13 09:50:52 +0700

    Micropost2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   root 'users#index'
+#DemoApp::Application.route.draw do
+#resources :users
+#resources :users
+#resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#end
 end
